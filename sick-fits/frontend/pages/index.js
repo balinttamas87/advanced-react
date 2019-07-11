4553e3471503b3faafdd094aa6083c9561d872ce
Tamas Balint <balint.tamas87@gmail.com>
2019-07-11 13:38:44 +0100

    Query all items and render them

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,48 @@
-import Link from "next/link";
+import Items from "../components/Items";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "../components/Item";
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+const Center = styled.div`
+  text-align: center;
+`;
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
 
 const Home = props => (
-  <div>
-    <p>Hey!</p>
-  </div>
+  <Center>
+    <Query query={ALL_ITEMS_QUERY}>
+      {({ data, error, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error: ${error.message}</p>;
+        return (
+          <ItemsList>
+            {data.items.map(item => (
+              <Item item={item} key={item.id} />
+            ))}
+          </ItemsList>
+        );
+      }}
+    </Query>
+  </Center>
 );
 
 export default Home;
