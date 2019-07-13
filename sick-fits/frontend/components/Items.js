03a5cb856d9abc19fbdfa48d0922705818e9a88d
Tamas Balint <balint.tamas87@gmail.com>
2019-07-13 15:24:16 +0100

    Move Items component into its own file

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,14 +1,49 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "../components/Item";
 
-class Items extends Component {
-  render() {
-    return (
-      <div>
-        <p>Items</p>
-      </div>
-    );
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
   }
-}
+`;
 
-export default Items;
+const Center = styled.div`
+  text-align: center;
+`;
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
+const Home = props => (
+  <Center>
+    <Query query={ALL_ITEMS_QUERY}>
+      {({ data, error, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error: ${error.message}</p>;
+        return (
+          <ItemsList>
+            {data.items.map(item => (
+              <Item item={item} key={item.id} />
+            ))}
+          </ItemsList>
+        );
+      }}
+    </Query>
+  </Center>
+);
+
+export default Home;
+export { ALL_ITEMS_QUERY };
