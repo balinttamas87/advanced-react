af17027d8db32b13f130e7b86825b5a48902af78
Tamas Balint <balint.tamas87@gmail.com>
2019-07-14 15:53:50 +0100

    Sign up a user using JWT cookie

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,4 @@
-// let's go!
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
@@ -6,6 +6,7 @@ const db = require("./db");
 const server = createServer();
 
 // TODO user express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO user express middleware to populate current user
 
 server.start(
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem(
@@ -32,6 +35,41 @@ const Mutations = {
     // TODO
     // delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+
+    // hash password
+    const password = await bcrypt.hash(args.password, 10);
+
+    // create user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+
+    // create JWT token for user
+    const token = jwt.sign(
+      {
+        userId: user.id
+      },
+      process.env.APP_SECRET
+    );
+
+    // set a jwt cookie on the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+
+    // return user to the browser
+    return user;
   }
 };
 
