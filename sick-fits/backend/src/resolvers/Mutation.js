3d720b731f57d07556bb54189a4f60db80b07d1c
Tamas Balint <balint.tamas87@gmail.com>
2019-07-15 12:54:41 +0100

    Reset password and send reset email

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { randomBytes } = require("crypto");
 const { promisify } = require("util");
+const { transport, makeANiceEmail } = require("../mail");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -115,9 +116,21 @@ const Mutations = {
         resetTokenExpiry
       }
     });
-    console.log({ res });
-    return { message: "Thanks!" };
     // email the reset token
+    const mailRes = await transport.sendMail({
+      from: "tamas@balint.com",
+      to: user.email,
+      subject: "Your Password Reset Token",
+      html: makeANiceEmail(`
+        Your password reset token is here!
+        \n\n
+        <a href="${
+          process.env.FRONTEND_URL
+        }/reset?resetToken=${resetToken}">Click here to reset</a>
+      `)
+    });
+
+    return { message: "Thanks!" };
   },
   async resetPassword(parent, args, ctx, info) {
     // check if the passwords match
